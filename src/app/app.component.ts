406fc9afc133d96f56f1110d8893f0f25db9ba45
sandhya <pagillasandhya1234@gmail.com>
2023-01-17 15:40:44 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {of, from } from 'rxjs';
+import { observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Rxjs9';
-}
+  ngOnInit() {
+  // of(2,4,6,8).subscribe(item=> console.log(item));
+  // from([20, 15,10, 5]).subscribe({
+  //   next: (item) => console.log(`resulting item ..${item}`),
+  //   error: (err) => console.error('error occured ${err}'),
+  //   complete:() => console.log('complete'),
+  // });
+      //  of('Apple1','Apple2', 'Apple3') .subscribe{{
+      //   next: (item) => console.log('resulting item..${item}'),
+      //   error: (err) => console.error{'error occured ${err}'}
+        
+       }}
+  
+  
+  
+
+
+
+
